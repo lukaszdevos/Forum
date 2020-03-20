4ddfd9da5707ff813b506f0775dfd4b57fa5d466
lukaszdevos <50773841+lukaszdevos@users.noreply.github.com>
2020-03-20 19:29:23 +0100

    change allowed_host local host...

--- ForumProject/settings.py
+++ ForumProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['forum-project-app.herokuapp.com']
+ALLOWED_HOSTS = ['forum-project-app.herokuapp.com','127.0.0.1',"localhost", "0.0.0.0", ".herokuapp.com"]
 
 # Application definition
 

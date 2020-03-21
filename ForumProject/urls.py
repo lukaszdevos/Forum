65006bb65008b805492f92ac5ab388482deec0e5
lukaszdevos <50773841+lukaszdevos@users.noreply.github.com>
2020-03-21 19:30:19 +0100

    media root config

--- ForumProject/urls.py
+++ ForumProject/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from accounts import views
 from . import views
 from news.views import scrape_news
 

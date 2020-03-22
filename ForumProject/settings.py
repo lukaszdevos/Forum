399cdc08775afac4fa80147ef9caa6273305d8fd
lukaszdevos <50773841+lukaszdevos@users.noreply.github.com>
2020-03-22 19:21:08 +0100

    asw s3 region

--- ForumProject/settings.py
+++ ForumProject/settings.py
@@ -162,7 +162,7 @@ AWS_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 
-AWS_S3_REGION_NAME = "ap-south-1"
+AWS_S3_REGION_NAME = "eu-central-1"
 
 AWS_S3_SIGNATURE_VERSION = "s3v4"
 

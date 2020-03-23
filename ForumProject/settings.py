c235f142bc9623ada999c683bdd8b430286b5332
lukaszdevos <50773841+lukaszdevos@users.noreply.github.com>
2020-03-23 21:33:35 +0100

    add sendgrid to mail

--- ForumProject/settings.py
+++ ForumProject/settings.py
@@ -175,14 +175,14 @@ LOGIN_URL = 'accounts/login'
 
 
 #email config
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS = True
 
 
 
